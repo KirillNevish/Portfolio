d78383d9b49bb00accbce19bbe479ac39340c635
KirillNevish <kirillvish482@gmail.com>
2025-04-17 15:35:14 +0300

    Portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,22 @@ import AnimatedGif from './animatedGif';
 const Header = dynamic(() => import('./header/page'), { ssr: true });
 import Footer from './footer/page';
 import LeftSidebar from './leftSidebar/page';
-
+import { motion } from 'framer-motion';
 
 
 
 export default function Home() {
+
+  const paragraphs = [
+    "I’m a passionate Front-End Developer with a love for design and technology. My journey into web development began with a curiosity to create something both functional and visually appealing. Before diving into Front-End Development, I explored various creative tools like Photoshop and Blender, but I found my true calling in building interactive and dynamic web applications.",
+    "My foundational skills include HTML, CSS, JavaScript, and React, which I honed through a comprehensive development course. Beyond the course, I've expanded my expertise by learning additional languages, libraries and frameworks such as Next.js, Tailwind, and Vue.js and more. I’m particularly fascinated by the process of optimizing projects—transforming a simple page into a fully functional, high-performance website, and continually testing and enhancing it with new features.",
+    "One of my proudest achievements is a Clothing Store project I developed as the capstone of my course. This was my first large-scale project, and it challenged me to leverage all my skills to build a complete, working e-commerce system. While I haven’t yet participated in group projects, I’m eager to collaborate and gain new experiences in team settings.",
+    "I stay updated on the latest trends and technologies in Front-End Development by watching educational videos and tutorials. Though the journey has had its challenges—particularly when adding new features—I’ve learned that a methodical approach to problem-solving is key. By stepping back, evaluating potential impacts, and then moving forward with a clear plan, I’ve found that even the most complex issues can be resolved efficiently.",
+    "Outside of development, I have a strong interest in music and enjoy playing the piano in my free time. This creative outlet complements my work in coding, where I start by designing a basic, non-functional interface and then refine and enhance it with additional features.",
+    "Looking ahead, I’m committed to furthering my skills not only in Front-End Development but across the entire IT landscape. My goal is to work on creative and social projects that make a positive impact, continuing to push the boundaries of what I can achieve as a developer."
+  ];
+
+
   return (
     <div id={style.mainbg} className="pt-5 pb-5 d-flex justify-content-center">
       <div id={style.blurContainer} ></div>
@@ -41,28 +52,24 @@ export default function Home() {
         <div className="row featurette bg-body bg-opacity-10 rounded-4 shadow-lg p-3 mb-5 mt-5 rounded" bis_skin_checked="1">
           <div id={style.aboutMeInfo} className="col-md-7 mt-2" bis_skin_checked="1">
             <h2 className="featurette-heading fw-normal lh-1 text-white">About me<span className="text-body-secondary"></span></h2>
-            <p className="lead text-white-50">I’m a passionate Front-End Developer with a love for design and technology. My journey into web development began with a curiosity to create something both functional and visually appealing. Before diving into Front-End Development, I explored various creative tools like Photoshop and Blender, but I found my true calling in building interactive and dynamic web applications.
-            </p>
-            <p className="lead text-white-50">
-              My foundational skills include HTML, CSS, JavaScript, and React, which I honed through a comprehensive development course. Beyond the course, I've expanded my expertise by learning additional languages, libraries and frameworks such as Next.js, Tailwind, and Vue.js and more. I’m particularly fascinated by the process of optimizing projects—transforming a simple page into a fully functional, high-performance website, and continually testing and enhancing it with new features.
-            </p>
-            <p className="lead text-white-50">
-              One of my proudest achievements is a Clothing Store project I developed as the capstone of my course. This was my first large-scale project, and it challenged me to leverage all my skills to build a complete, working e-commerce system. While I haven’t yet participated in group projects, I’m eager to collaborate and gain new experiences in team settings.
-            </p>
-            <p className="lead text-white-50">
-              I stay updated on the latest trends and technologies in Front-End Development by watching educational videos and tutorials. Though the journey has had its challenges—particularly when adding new features—I’ve learned that a methodical approach to problem-solving is key. By stepping back, evaluating potential impacts, and then moving forward with a clear plan, I’ve found that even the most complex issues can be resolved efficiently.
-            </p>
-            <p className="lead text-white-50">
-              Outside of development, I have a strong interest in music and enjoy playing the piano in my free time. This creative outlet complements my work in coding, where I start by designing a basic, non-functional interface and then refine and enhance it with additional features.
-            </p>
-            <p className="lead text-white-50">
-              Looking ahead, I’m committed to furthering my skills not only in Front-End Development but across the entire IT landscape. My goal is to work on creative and social projects that make a positive impact, continuing to push the boundaries of what I can achieve as a developer.
-            </p>
+            {paragraphs.map((text, index) => (
+            <motion.p
+              key={index}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: index * 0.2, duration: 0.6 }}
+              viewport={{ once: true, amount: 0.3 }}
+              className="lead text-white-50"
+            >
+              {text}
+            </motion.p> 
+            ))}
           </div>
           <div id={style.widerGif} className="col-md-5 mt-2 rounded-4" bis_skin_checked="1">
             <AnimatedGif src="/programming-frog.gif" alt="frog coding" />
           </div>
         </div>
+
 
         <hr className="featurette-divider text-white" />
 
